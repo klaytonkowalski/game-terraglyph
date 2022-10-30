6668ef62a1df0b7579575ff034784e398937fb09
Klayton Kowalski <klaytonkowalski@gmail.com>
2022-10-30 19:38:32 -0400

    Updates.

---
+++ modules/dtile_animation_groups.lua
@@ -0,0 +1,9 @@
+return
+{
+	[1] =
+	{
+		[2] = { sequence = { 2, 3 }, trigger = false, frequency = 2 },
+		[9] = { sequence = { 9, 10, 9, 11 }, trigger = false, frequency = 4 },
+		[12] = { sequence = { 12, 13 }, trigger = false, frequency = 2 }
+	}
+}
