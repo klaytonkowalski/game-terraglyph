a38389a79ef2e8b596ad19143a4d70d957080814
Klayton Kowalski <klaytonkowalski@gmail.com>
2022-11-05 00:52:38 -0400

    Updates.

--- dtypewriter/dtypewriter.lua
+++ dtypewriter/dtypewriter.lua
@@ -37,7 +37,6 @@ local _container_node
 local _font_id
 local _font
 
-local _text_raw
 local _text_area_x
 local _text_area_y
 local _text_area_width
@@ -47,12 +46,18 @@ local _line_offset
 
 local _chunks = {}
 
-local _character_nodes = {}
+local _characters = {}
+local _character_index
+local _paragraph_index
+
+local _waiting = false
 
 local _colors = {}
 local _default_color = vmath.vector4()
 
-local _messages_toggle = false
+local _speed_max = 120
+local _speed = 30
+
 local _messages_url
 
 ----------------------------------------------------------------------
@@ -61,9 +66,11 @@ local _messages_url
 
 dtypewriter.messages =
 {
-	text_start = hash("text_start"),
-	text_end = hash("text_end"),
-	type = hash("type")
+	start = hash("start"),
+	type = hash("type"),
+	wait = hash("wait"),
+	continue = hash("continue"),
+	complete = hash("complete")
 }
 
 ----------------------------------------------------------------------
@@ -78,52 +85,45 @@ local function strip_spaces(text)
 	return remove_br_spaces
 end
 
-local function add_chunks(text)
-	local start_index = 1
-	local character_index = 1
-	local color = _default_color
-	while character_index <= #text do
-		local character = string.sub(text, character_index, character_index)
-		if character == " " then
-			local chunk_1 = { text = string.sub(text, start_index, character_index - 1), color = color, content = true }
-			_chunks[#_chunks + 1] = chunk_1
-			local chunk_2 = { text = " ", color = color, space = true }
-			_chunks[#_chunks + 1] = chunk_2
-			start_index = character_index + 1
-			character_index = character_index + 1
-		elseif character == "<" then
-			if string.sub(text, character_index, character_index + 6) == "<color=" then
-				local color_start_index, color_end_index = string.find(text, "%l+", character_index + 7)
-				local color_name = string.sub(text, color_start_index, color_end_index)
-				color = _colors[color_name] or _default_color
-				text = string.sub(text, 1, character_index - 1) .. string.sub(text, color_end_index + 2)
-			elseif string.sub(text, character_index, character_index + 3) == "<br>" then
-				local chunk_1 = { text = string.sub(text, start_index, character_index - 1), color = color }
-				_chunks[#_chunks + 1] = chunk_1
-				local chunk_2 = { br = true }
-				_chunks[#_chunks + 1] = chunk_2
-				start_index = character_index + 4
-				character_index = character_index + 4
-			end
-		elseif character_index == #text then
-			local chunk = { text = string.sub(text, start_index, character_index), color = color, content = true }
-			_chunks[#_chunks + 1] = chunk
-			character_index = character_index + 1
-		else
-			character_index = character_index + 1
-		end
-	end
+local function add_chunk(type, text)
+	local chunk = { type = type, text = text }
+	_chunks[#_chunks + 1] = chunk
+end
+
+local function add_character(chunk_index, text, color, speed)
+	local character_data = { chunk_index = chunk_index, text = text, color = color, speed = speed }
+	_characters[#_characters + 1] = character_data
 end
 
 local function set_transparent(color)
 	return vmath.vector4(color.x, color.y, color.z, 0)
 end
 
+local function type_callback()
+	msg.post(_messages_url, dtypewriter.messages.type)
+	local character_data = _characters[_character_index]
+	gui.set_color(character_data.node, character_data.color)
+	_character_index = _character_index + 1
+	local next_character_data = _characters[_character_index]
+	if next_character_data then
+		if character_data.paragraph < next_character_data.paragraph then
+			msg.post(_messages_url, dtypewriter.messages.wait)
+			_waiting = true
+		else
+			timer.delay(1 / next_character_data.speed, false, type_callback)
+		end
+	else
+		msg.post(_messages_url, dtypewriter.messages.complete)
+		_character_index = nil
+		_paragraph_index = nil
+	end
+end
+
 ----------------------------------------------------------------------
 -- MODULE FUNCTIONS
 ----------------------------------------------------------------------
 
-function dtypewriter.init(container_node_id, font_id, text_area_x, text_area_y, text_area_width, line_count_max, line_offset)
+function dtypewriter.init(container_node_id, font_id, text_area_x, text_area_y, text_area_width, line_count_max, line_offset, messages_url)
 	dtypewriter.clear()
 	_container_node = gui.get_node(container_node_id)
 	_font_id = font_id
@@ -133,50 +133,105 @@ function dtypewriter.init(container_node_id, font_id, text_area_x, text_area_y, 
 	_text_area_width = text_area_width
 	_line_count_max = line_count_max
 	_line_offset = line_offset
+	_messages_url = messages_url
 end
 
 function dtypewriter.clear()
 	_text_raw = nil
 	_chunks = {}
-	for _, node in pairs(_character_nodes) do
-		gui.delete_node(node)
+	for _, character_data in ipairs(_characters) do
+		gui.delete_node(character_data.node)
 	end
-	_character_nodes = {}
+	_characters = {}
+	_character_index = nil
+	_paragraph_index = nil
+	_waiting = false
 end
 
 function dtypewriter.load(text)
 	dtypewriter.clear()
-	_text_raw = text
 	text = strip_spaces(text)
+	local chunk_start_index = 1
+	local character_index = 1
+	local character_color = _default_color
+	local character_speed = _speed
+	while character_index <= #text do
+		local character = string.sub(text, character_index, character_index)
+		if character == " " then
+			local chunk_type = "content"
+			local chunk_text = string.sub(text, chunk_start_index, character_index - 1)
+			add_chunk(chunk_type, chunk_text)
+			chunk_text = " "
+			chunk_type = "space"
+			add_chunk(chunk_type, chunk_text)
+			add_character(#_chunks, " ", character_color, character_speed)
+			chunk_start_index = character_index + 1
+			character_index = character_index + 1
+		elseif character == "<" then
+			if string.sub(text, character_index, character_index + 6) == "<color=" then
+				local color_start_index, color_end_index = string.find(text, "%l+", character_index + 7)
+				local color_name = string.sub(text, color_start_index, color_end_index)
+				character_color = (color_name == "default" or not _colors[color_name]) and _default_color or _colors[color_name]
+				text = string.sub(text, 1, character_index - 1) .. string.sub(text, color_end_index + 2)
+			elseif string.sub(text, character_index, character_index + 6) == "<speed=" then
+				local speed_start_index, speed_end_index = string.find(text, "%d*%l*", character_index + 7)
+				local speed_text = string.sub(text, speed_start_index, speed_end_index)
+				character_speed = speed_text == "default" and _speed or math.min(_speed_max, math.max(0, speed_text))
+				text = string.sub(text, 1, character_index - 1) .. string.sub(text, speed_end_index + 2)
+			elseif string.sub(text, character_index, character_index + 3) == "<br>" then
+				local chunk_type = "content"
+				local chunk_text = string.sub(text, chunk_start_index, character_index - 1)
+				add_chunk(chunk_type, chunk_text)
+				chunk_type = "br"
+				add_chunk(chunk_type)
+				chunk_start_index = character_index + 4
+				character_index = character_index + 4
+			end
+		else
+			add_character(#_chunks + 1, character, character_color, character_speed)
+			if character_index == #text then
+				local chunk_type = "content"
+				local chunk_text = string.sub(text, chunk_start_index, character_index)
+				add_chunk(chunk_type, chunk_text)
+			end
+			character_index = character_index + 1
+		end
+	end
 	local text_metrics = resource.get_text_metrics(_font, text)
-	local total_line_count = math.floor(text_metrics.width / (_text_area_width + 1) + 1)
-	add_chunks(text)
+	local paragraph = 1
+	local line = 1
 	local line_width_remaining = _text_area_width
-	local current_line = 1
-	local next_character_x = _text_area_x
-	for _, chunk in ipairs(_chunks) do
+	local character_x = _text_area_x
+	for chunk_index, chunk in ipairs(_chunks) do
 		local chunk_metrics = resource.get_text_metrics(_font, chunk.text or "")
-		if line_width_remaining - chunk_metrics.width < 0 or chunk_metrics.width == 0 then
+		if line_width_remaining - chunk_metrics.width < 0 or chunk.type == "br" then
+			line = line + 1
 			line_width_remaining = _text_area_width
-			current_line = current_line + 1
-			next_character_x = _text_area_x
-			if chunk.space then
+			if line > _line_count_max then
+				line = 1
+				paragraph = paragraph + 1
+			end
+			character_x = _text_area_x
+			if chunk.type == "space" then
 				goto continue
 			end
 		end
 		if chunk.text then
-			for character in string.gmatch(chunk.text, ".") do
-				local character_metrics = resource.get_text_metrics(_font, character)
-				local position = vmath.vector3(next_character_x, -_text_area_y - (current_line - 1) * text_metrics.height + (current_line - 1) * _line_offset, 0)
-				next_character_x = next_character_x + character_metrics.width + 1
-				line_width_remaining = line_width_remaining - character_metrics.width - 1
-				local character_node = gui.new_text_node(position, character)
-				gui.set_parent(character_node, _container_node)
-				gui.set_font(character_node, _font_id)
-				gui.set_color(character_node, chunk.color)
-				gui.set_adjust_mode(character_node, gui.ADJUST_FIT)
-				gui.set_pivot(character_node, gui.PIVOT_NW)
-				_character_nodes[#_character_nodes + 1] = character_node
+			for _, character_data in ipairs(_characters) do
+				if character_data.chunk_index == chunk_index then
+					local character_metrics = resource.get_text_metrics(_font, character_data.text)
+					local character_position = vmath.vector3(character_x, -_text_area_y - (line - 1) * text_metrics.height + (line - 1) * _line_offset, 0)
+					character_x = character_x + character_metrics.width + 1
+					line_width_remaining = line_width_remaining - character_metrics.width - 1
+					local character_node = gui.new_text_node(character_position, character_data.text)
+					gui.set_parent(character_node, _container_node)
+					gui.set_font(character_node, _font_id)
+					gui.set_color(character_node, set_transparent(character_data.color))
+					gui.set_adjust_mode(character_node, gui.ADJUST_FIT)
+					gui.set_pivot(character_node, gui.PIVOT_NW)
+					character_data.node = character_node
+					character_data.paragraph = paragraph
+				end
 			end
 		end
 		::continue::
@@ -184,32 +239,55 @@ function dtypewriter.load(text)
 end
 
 function dtypewriter.start()
-	if _character_nodes > 0 then
-		if _messages_toggle then
-			msg.post(_messages_url, dtypewriter.messages.text_start)
+	if #_characters > 0 and not _character_index then
+		msg.post(_messages_url, dtypewriter.messages.start)
+		_character_index = 1
+		_paragraph_index = 1
+		if _speed == 0 then
+			
+		else
+			timer.delay(1 / _speed, false, type_callback)
 		end
-		-- todo
+	end
+end
+
+function dtypewriter.continue()
+	if _waiting then
+		msg.post(_messages_url, dtypewriter.messages.continue)
+		_waiting = false
+		_paragraph_index = _paragraph_index + 1
+		character_index = 1
+		while character_index < _character_index do
+			local character_data = _characters[character_index]
+			gui.set_color(character_data.node, set_transparent(character_data.color))
+			character_index = character_index + 1
+		end
+		timer.delay(1 / _speed, false, type_callback)
 	end
 end
 
 function dtypewriter.set_color(name, color)
-	_colors[name] = set_transparent(color)
+	_colors[name] = color
 end
 
 function dtypewriter.set_default_color(color)
-	_default_color = set_transparent(color)
+	_default_color = color
 end
 
 function dtypewriter.clear_colors()
 	_colors = {}
 end
 
-function dtypewriter.toggle_messages(flag)
-	_messages_toggle = flag
+function dtypewriter.set_speed(speed)
+	_speed = math.min(_speed_max, math.max(0, speed))
 end
 
-function dtypewriter.set_messages_url(url)
-	_messages_url = url
+function dtypewriter.is_typing()
+	return _character_index and true or false
+end
+
+function dtypewriter.is_waiting()
+	return _waiting
 end
 
 return dtypewriter
